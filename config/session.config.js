c323b363035b66b7f909129c07097155c1a7d0c4
Diana Pena <dianaligiapena@gmail.com>
2023-02-10 19:10:29 +0200

    routest almost finished

--- config/session.config.js
+++ config/session.config.js
@@ -24,10 +24,10 @@ module.exports = app => {
                 sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
                 secure: process.env.NODE_ENV === 'production',
                 httpOnly: true,
-                maxAge: 60000 // 60 * 1000 ms === 1 min
+                maxAge: 60000 * 60 // 60 * 1000 ms === 1 min
             },
             store: MongoStore.create({
-                mongoUrl: process.env.MONGODB_URI || 'mongodb://localhost/cookiesAgain',
+                mongoUrl: process.env.MONGODB_URI || 'mongodb://localhost/w6-hogwarts-remote',
             }),
         })
     )
--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -4,10 +4,11 @@ const router = express.Router();
 
 /* GET Hogwarts home page */
 router.get("/", isLoggedIn, (req, res, next) => {
-    res.render("profile/profile-home", { user: req.session.user,
-        layout: "../views/layouts/profile-layout.ejs" });
+    res.render("profile/profile-home", { 
+        user: req. session.user,
+        layout: "../views/layouts/profile-layout.ejs" }
+    );
     
-
     console.log('SESSION =====> ', req.session);
 
     if (!req.session.user) {
@@ -15,12 +16,20 @@ router.get("/", isLoggedIn, (req, res, next) => {
     }
 });
 
+router.get("/profile-info", isLoggedIn, (req, res, next) => {
+    res.render("profile/profile-info", { 
+        user: req.session.user,
+        layout: "../views/layouts/profile-layout.ejs" });
+});
 
+router.get("/lounge", isLoggedIn, (req, res, next) => {
+    res.render("profile/lounge", { user: req.session.user, layout: "../views/layouts/profile-layout.ejs" })
+});
 
 router.get('/logout', isLoggedIn, (req, res) => {
     req.session.destroy(err => {
         if (err) next(err)
-        res.redirect('/')
+        res.redirect('/');
     })
 })
 
