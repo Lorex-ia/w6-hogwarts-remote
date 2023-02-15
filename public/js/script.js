6b4697e67a3e845920aa6d62d338362ccb3e87fb
Diana Pena <dianaligiapena@gmail.com>
2023-02-15 23:06:25 +0200

    welcome page mostly done

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,17 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/Window/DOMContentLoaded_event
+// -> event fires when the HTML document has been completely parsed, and all deferred scripts (<script defer src="…"> and <script type="module">) have downloaded and executed. It doesn't wait for other things like images, subframes, and async scripts to finish loading.
+
+//the hogwarts music library - another vesrion is with embed in index layout
+
+// const song = new Audio();    
+// song.src = " audio/HogwartsMusic.mp3 " ;
+// song.volume = 0.3 ;
+
 document.addEventListener("DOMContentLoaded", () => {
   console.log("w6-hogwarts-remote JS imported successfully!");
+
+  //play hogwarts music
+
+  // song.play();
 });
+
